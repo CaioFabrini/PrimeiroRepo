ac55137a5199f0c434c17c646e71c94bc149505c
Caio Fabrini <caiofabrini@outlook.com>
2025-06-08 14:16:18 +0100

    add a new project

---
+++ MeuPrimeiroProjetoRepo/MeuPrimeiroProjetoRepo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MeuPrimeiroProjetoRepo
+//
+//  Created by Caio Fabrini on 08/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+  
+}
+
